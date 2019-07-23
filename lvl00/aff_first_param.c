120728b16e0f2e83574218000395815ff70b7dec
root <root@DESKTOP-KUSDV8C.localdomain>
2019-07-23 21:28:20 +0200

    changes to param

--- lvl00/aff_first_param.c
+++ lvl00/aff_first_param.c
@@ -1,21 +1,20 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
 	int i;
 
 	i = 0;
-	
-	if(argc == 1)
+	if (argc > 1)
 	{
-		write(1, "\n", 1);
-	}
-	if(argc >= 2)
-	{
-		while(argv[1][i] != '\0')
+		while (argv[1][i])
 		{
 			write(1, &argv[1][i++], 1);
 		}
+		write (1, "\n", 1);
+	}
+	else
+	{
 		write(1, "\n", 1);
 	}
 	return (0);
