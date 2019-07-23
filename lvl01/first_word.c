b0c671586cb723f4607b6ed32f5350cb35698e28
root <root@DESKTOP-KUSDV8C.localdomain>
2019-07-23 21:31:00 +0200

    changes made

--- lvl01/first_word.c
+++ lvl01/first_word.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   first_word.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/30 06:51:30 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/04/30 06:53:47 by kfrancis         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
 	int i;
 
@@ -20,11 +8,17 @@ int main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		while(*argv[1] == ' ' && *argv[1] == '\t')
-			argv[1][i]++;
-		while (*argv[1] != '\t' && *argv[1] != '\0' && *argv[1] != ' ')
-			write(1, &*argv[1]++, 1);
+		while (argv[1][i])
+		{
+			while (argv[1][i] == '\t' || argv[1][i] == ' ' || argv[1][i] == '\0')
+				i++;
+			while (argv[1][i] != '\t' || argv[1][i] != ' ' || argv[1][i] != '\0')
+			{
+				write (1, &argv[1][i++], 1);
+			}
+			break;
+	  	}
+
 	}
-		write(1, "\n", 1);
-		return (0);
+	write(1, "\n", 1);
 }
